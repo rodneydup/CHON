570632995ec6556e1307e81f826c3eb13cace1a4
Rodney DuPlessis <rodney@rodneyduplessis.com>
2021-03-27 17:19:32 -0400

    more macos build stuff

--- configure.sh
+++ configure.sh
@@ -7,14 +7,21 @@ git submodule update --init --recursive
   cd build
   mkdir -p release
   cd release
-  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=1 -DRTMIDI_API_JACK=1 ../..
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
+  elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "CONFIGURE WINDOWS RELEASE TODO"
+    cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -DRTAUDIO_API_JACK=0 -DRTMIDI_API_JACK=0 ../..
+  fi
 )
 
 # Configure debug build
-(
-  mkdir -p build
-  cd build
-  mkdir -p debug
-  cd debug
-  cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
-)
+# (
+#   mkdir -p build
+#   cd build
+#   mkdir -p debug
+#   cd debug
+#   cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+# )
