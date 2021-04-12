0d95ca27022006109c2271b0bf0b79b6eae9bf55
Rodney DuPlessis <rodney@rodneyduplessis.com>
2021-04-12 16:12:17 -0400

    some changes for windows build

--- distclean.sh
+++ distclean.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 rm -rf build
-
-# currently not purging bin directory in case you need dynamic libraries or other config files with the binary
-# rm -rf bin
-
-(
-  cd allolib
-  ./distclean
-)
